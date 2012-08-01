44e8af3b0307ab4527b5665c94295ae6d05076dc
Arjun Raj <rajarju@gmail.com>
2012-08-01 15:38:37 +0530

    More Demos + Slides

--- sample/worker/script.js
+++ sample/worker/script.js
@@ -1,155 +1,49 @@
-
-window.requestAnimationFrame = (function() {
-  //Check for each browser
-  //@paul_irish function
-  //Globalises this function to work on any browser as each browser has a different namespace for this
-  return window.requestAnimationFrame || //Chromium
-  window.webkitRequestAnimationFrame || //Webkit
-  window.mozRequestAnimationFrame || //Mozilla Geko
-  window.oRequestAnimationFrame || //Opera Presto
-  window.msRequestAnimationFrame || //IE Trident?
-
-
-  function(callback, element) { //Fallback function
-    window.setTimeout(callback, 1000 / 60);
-  }
-
-})();
-
-
-var canvas, context, oldTime, frametime;
-
-var balls = [];
-
-function ball(){
-  this.init =  function(x, y, dx, dy, r) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.r = r;
-  }
-  this.x = 10;
-  this.y = 10;
-  this.dx = 4;
-  this.dy = 4;
-  this.r = 10;
-  this.id = balls.length;
-  this.draw = function() {
-    //this.move();
-    context.fillStyle = 'rgb(0, 0, 0)';
-    context.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
-    context.closePath();
-    context.fill();
-  }
-  this.move = function() {
-    console.log(this.x, this.y)
-     worker.postMessage({
-      'cmd': 'update', 
-      'ball': {
-        x:  this.x,
-        y:  this.y,
-        dx: this.dx,
-        dy: this.dy,
-        r:  this.r ,
-        id: this.id
-      }
-    });
-    // if ((this.x - this.r) < 0 || (this.x + this.r) > canvas.width) {
-    //   this.dx = -1 * this.dx;
-    // }
-    // if ((this.y - this.r) < 0 || (this.y + this.r) > canvas.height) {
-    //   this.dy = -1 * this.dy;
-    // }
-
-    // this.x += this.dx;
-    // this.y += this.dy;
-
-  }
-  this.update = function(x, y){
-    this.x = x;
-    this.y = y;
-  }
-
-};
-
-var clear = function() {
-  canvas.width = 600;
-}
-
-function init() {
-  canvas = document.getElementById('canvas');
-   worker.postMessage({
-      'cmd': 'start', 
-      'settings': {
-        canvas : {
-          height: canvas.height,
-          width: canvas.width,
-        }
-      }
-    });
-
-  context = canvas.getContext('2d');
-  balls.push(new ball());
-  oldTime = new Date().getTime();
-}
-
-function animate() {
-  clear();
-  //balls[0].draw();
-  for(i in balls){
-    balls[i].move();
-  }
-  
-  fps();
-  //console.log(balls);
-  //window.setTimeout(animate, 1000/60);
-  if(action == true){
-    window.requestAnimationFrame(animate);
-  }
-}
-
-function addBall(){
-  //alert('Addijg');
-  balls.push(new ball());
-}
-function removeBall(){
-  //alert('Addijg');
-  balls.pop();
-}
-
-
-function fps(){
-  var time = new Date().getTime();
-  frametime = 1/(time - oldTime) * 100000/60;
-  oldTime = time;
-}
-
-
 var worker = new Worker('worker.js');
 
 worker.addEventListener('message', function(e) {
-
   switch(e.data.cmd){
-    case 'update':      
-      balls[e.data.ball.id].update(e.data.ball.x, e.data.ball.y)
-      balls[e.data.ball.id].draw();      
+    case 'result':      
+            $.get('values').value = e.data.result;
     break;
   }
 
 }, false);
 
 
-window.onload = function(){
-  action = true;
-  init();
-  animate();
+var $ = {};
+$.get = function(ele){
+  return document.getElementById(ele);
+};
+
+
+function generate(){
+  var values = [];
+  var count = $.get('count').value;
+  for (var i = 0; i <= count; i++) {
+    var randomnumber= Math.floor(Math.random()*11 + Math.random()*100 + Math.random()*1000);
+    values.push(randomnumber);
+  };
+  $.get('values').value = values.join(',');
 }
 
+function addBall(){
+  var slider = document.createElement('DIV');
+  slider.className = 'slider';  
+  var ball = document.createElement('DIV');
+  ball.className = 'puck';
+  slider.appendChild(ball);
+  $.get('clock').appendChild(slider);
+}
 
-/** FPS CALCULATION **/
-setInterval(function(){
-  document.getElementById('fps').innerHTML = parseInt(frametime);
-  document.getElementById('balls').innerHTML = balls.length;
-},1000)
+function normalSort(){
+  var digiarray = $.get('values').value.split(',');
+  digiarray.sort(function(a,b){return a - b});
+  $.get('values').value = digiarray.join(',');
+}
 
+function workerSort(){
+  worker.postMessage({
+    cmd : 'sort',
+    data : $.get('values').value
+  });
+}
--- sample/worker/worker.js
+++ sample/worker/worker.js
@@ -1,35 +1,14 @@
-var settings = {};
-
 self.addEventListener('message', function(e) {
   var data = e.data;
   switch (data.cmd) {
-    case 'start':
-      //self.postMessage('WORKER STARTED: ' + data.msg);
-      settings = data.settings;
-      break;
+    case 'sort':
 
-    case 'update':
-    	
-    	if ((data.ball.x - data.ball.r) < 0 || (data.ball.x + data.ball.r) > settings.canvas.width) {
-	      data.ball.dx = -1 * data.ball.dx;
-	    }
-	    if ((data.ball.y - data.ball.r) < 0 || (data.ball.y + data.ball.r) > settings.canvas.height) {
-	      data.ball.dy = -1 * data.ball.dy;
-	    }
-
-	    data.ball.x += data.ball.dx;
-	    data.ball.y += data.ball.dy;
-	    
 	    self.postMessage({
-	    	cmd : 'update',
-	    	ball: data.ball
+	    	cmd : 'result',
+	    	result: normalSort(data.data)
 	    });
     	break;
-
-    case 'stop':
-      self.postMessage('WORKER STOPPED');
-      self.close(); // Terminates the worker.
-      break;
+  
     default:
       self.postMessage('Unknown command: ' + data.msg);
   };
@@ -37,22 +16,8 @@ self.addEventListener('message', function(e) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-// if ((data.ball.x - data.ball.r) < 0 || (data.ball.x + data.ball.r) > canvas.width) {
-    //   data.ball.dx = -1 * data.ball.dx;
-    // }
-    // if ((data.ball.y - data.ball.r) < 0 || (data.ball.y + data.ball.r) > canvas.height) {
-    //   data.ball.dy = -1 * data.ball.dy;
-    // }
-
-    // data.ball.x += data.ball.dx;
-    // data.ball.y += data.ball.dy;
+function normalSort(digi){  
+  var digiarray = digi.split(',');
+  digiarray.sort(function(a,b){return a - b});
+  return digiarray.join(',');
+}
